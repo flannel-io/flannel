flannel-io/flannel

commit d9d89f6bde8dfeb8af252f39ee70e2ecfc379eee

subnet: add infrastructure and tests for network watches

subnet/etcd.go
@@ -19,6 +19,7 @@ import (
 	"errors"
 	"fmt"
 	"net"
+	"path"
 	"regexp"
 	"strconv"
 	"time"
@@ -35,7 +36,8 @@ const (
 )
 
 type EtcdManager struct {
-	registry Registry
+	registry     Registry
+	networkRegex *regexp.Regexp
 }
 
 var (
@@ -55,15 +57,21 @@ func NewEtcdManager(config *EtcdConfig) (Manager, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &EtcdManager{r}, nil
+	return &EtcdManager{
+		registry:     r,
+		networkRegex: regexp.MustCompile(config.Prefix + `/([^/]*)/config`),
+	}, nil
 }
 
 func newEtcdManager(r Registry) Manager {
-	return &EtcdManager{r}
+	return &EtcdManager{
+		registry:     r,
+		networkRegex: regexp.MustCompile(`/coreos.com/network/([^/]*)/config`),
+	}
 }
 
 func (m *EtcdManager) GetNetworkConfig(ctx context.Context, network string) (*Config, error) {
-	cfgResp, err := m.registry.getConfig(ctx, network)
+	cfgResp, err := m.registry.getNetworkConfig(ctx, network)
 	if err != nil {
 		return nil, err
 	}
@@ -280,11 +288,7 @@ func (m *EtcdManager) RenewLease(ctx context.Context, network string, lease *Lea
 	return nil
 }
 
-func (m *EtcdManager) WatchLeases(ctx context.Context, network string, cursor interface{}) (WatchResult, error) {
-	if cursor == nil {
-		return m.watchReset(ctx, network)
-	}
-
+func getNextIndex(cursor interface{}) (uint64, error) {
 	nextIndex := uint64(0)
 
 	if wc, ok := cursor.(watchCursor); ok {
@@ -293,13 +297,26 @@ func (m *EtcdManager) WatchLeases(ctx context.Context, network string, cursor in
 		var err error
 		nextIndex, err = strconv.ParseUint(s, 10, 64)
 		if err != nil {
-			return WatchResult{}, fmt.Errorf("failed to parse cursor: %v", err)
+			return 0, fmt.Errorf("failed to parse cursor: %v", err)
 		}
 	} else {
-		return WatchResult{}, fmt.Errorf("internal error: watch cursor is of unknown type")
+		return 0, fmt.Errorf("internal error: watch cursor is of unknown type")
 	}
 
-	resp, err := m.registry.watchSubnets(ctx, network, nextIndex)
+	return nextIndex, nil
+}
+
+func (m *EtcdManager) WatchLeases(ctx context.Context, network string, cursor interface{}) (LeaseWatchResult, error) {
+	if cursor == nil {
+		return m.leaseWatchReset(ctx, network)
+	}
+
+	nextIndex, err := getNextIndex(cursor)
+	if err != nil {
+		return LeaseWatchResult{}, err
+	}
+
+	resp, err := m.registry.watch(ctx, path.Join(network, "subnets"), nextIndex)
 
 	switch {
 	case err == nil:
@@ -307,10 +324,35 @@ func (m *EtcdManager) WatchLeases(ctx context.Context, network string, cursor in
 
 	case isIndexTooSmall(err):
 		log.Warning("Watch of subnet leases failed because etcd index outside history window")
-		return m.watchReset(ctx, network)
+		return m.leaseWatchReset(ctx, network)
 
 	default:
-		return WatchResult{}, err
+		return LeaseWatchResult{}, err
+	}
+}
+
+func (m *EtcdManager) WatchNetworks(ctx context.Context, cursor interface{}) (NetworkWatchResult, error) {
+	if cursor == nil {
+		return m.networkWatchReset(ctx)
+	}
+
+	nextIndex, err := getNextIndex(cursor)
+	if err != nil {
+		return NetworkWatchResult{}, err
+	}
+
+	resp, err := m.registry.watch(ctx, "", nextIndex)
+
+	switch {
+	case err == nil:
+		return m.parseNetworkWatchResponse(resp)
+
+	case isIndexTooSmall(err):
+		log.Warning("Watch of subnet leases failed because etcd index outside history window")
+		return m.networkWatchReset(ctx)
+
+	default:
+		return NetworkWatchResult{}, err
 	}
 }
 
@@ -319,10 +361,10 @@ func isIndexTooSmall(err error) bool {
 	return ok && etcdErr.Code == etcd.ErrorCodeEventIndexCleared
 }
 
-func parseSubnetWatchResponse(resp *etcd.Response) (WatchResult, error) {
+func parseSubnetWatchResponse(resp *etcd.Response) (LeaseWatchResult, error) {
 	sn, err := parseSubnetKey(resp.Node.Key)
 	if err != nil {
-		return WatchResult{}, fmt.Errorf("error parsing subnet IP: %s", resp.Node.Key)
+		return LeaseWatchResult{}, fmt.Errorf("error parsing subnet IP: %s", resp.Node.Key)
 	}
 
 	evt := Event{}
@@ -330,15 +372,16 @@ func parseSubnetWatchResponse(resp *etcd.Response) (WatchResult, error) {
 	switch resp.Action {
 	case "delete", "expire":
 		evt = Event{
-			SubnetRemoved,
+			EventRemoved,
 			Lease{Subnet: sn},
+			"",
 		}
 
 	default:
 		attrs := &LeaseAttrs{}
 		err := json.Unmarshal([]byte(resp.Node.Value), attrs)
 		if err != nil {
-			return WatchResult{}, err
+			return LeaseWatchResult{}, err
 		}
 
 		exp := time.Time{}
@@ -347,36 +390,120 @@ func parseSubnetWatchResponse(resp *etcd.Response) (WatchResult, error) {
 		}
 
 		evt = Event{
-			SubnetAdded,
+			EventAdded,
 			Lease{
 				Subnet:     sn,
 				Attrs:      attrs,
 				Expiration: exp,
 			},
+			"",
 		}
 	}
 
-	cursor := watchCursor{resp.Node.ModifiedIndex}
-
-	return WatchResult{
-		Cursor: cursor,
+	return LeaseWatchResult{
+		Cursor: watchCursor{resp.Node.ModifiedIndex},
 		Events: []Event{evt},
 	}, nil
 }
 
-// watchReset is called when incremental watch failed and we need to grab a snapshot
-func (m *EtcdManager) watchReset(ctx context.Context, network string) (WatchResult, error) {
-	wr := WatchResult{}
+// Returns network name from config key (eg, /coreos.com/network/foobar/config),
+// if the 'config' key isn't present we don't consider the network valid
+func (m *EtcdManager) parseNetworkKey(s string) (string, error) {
+	if parts := m.networkRegex.FindStringSubmatch(s); len(parts) == 2 {
+		return parts[1], nil
+	}
+
+	return "", errors.New("Error parsing Network key")
+}
+
+func (m *EtcdManager) parseNetworkWatchResponse(resp *etcd.Response) (NetworkWatchResult, error) {
+	netname, err := m.parseNetworkKey(resp.Node.Key)
+	if err != nil {
+		// Ignore non .../<netname>/config keys
+		return NetworkWatchResult{}, nil
+	}
+
+	evt := Event{}
+
+	switch resp.Action {
+	case "delete":
+		evt = Event{
+			EventRemoved,
+			Lease{},
+			netname,
+		}
+
+	default:
+		_, err := ParseConfig(resp.Node.Value)
+		if err != nil {
+			return NetworkWatchResult{}, err
+		}
+
+		evt = Event{
+			EventAdded,
+			Lease{},
+			netname,
+		}
+	}
+
+	return NetworkWatchResult{
+		Cursor: watchCursor{resp.Node.ModifiedIndex},
+		Events: []Event{evt},
+	}, nil
+}
+
+// getNetworks queries etcd to get a list of network names.  It returns the
+// networks along with the 'as-of' etcd-index that can be used as the starting
+// point for etcd watch.
+func (m *EtcdManager) getNetworks(ctx context.Context) ([]string, uint64, error) {
+	resp, err := m.registry.getNetworks(ctx)
+
+	networks := []string{}
+
+	if err == nil {
+		for _, node := range resp.Node.Nodes {
+			netname, err := m.parseNetworkKey(node.Key)
+			if err == nil {
+				networks = append(networks, netname)
+			}
+		}
+
+		return networks, resp.Index, nil
+	}
+
+	if etcdErr, ok := err.(etcd.Error); ok && etcdErr.Code == etcd.ErrorCodeKeyNotFound {
+		// key not found: treat it as empty set
+		return networks, etcdErr.Index, nil
+	}
+
+	return nil, 0, err
+}
+
+// leaseWatchReset is called when incremental lease watch failed and we need to grab a snapshot
+func (m *EtcdManager) leaseWatchReset(ctx context.Context, network string) (LeaseWatchResult, error) {
+	wr := LeaseWatchResult{}
 
 	leases, index, err := m.getLeases(ctx, network)
-
 	if err != nil {
 		return wr, fmt.Errorf("failed to retrieve subnet leases: %v", err)
 	}
 
-	cursor := watchCursor{index}
+	wr.Cursor = watchCursor{index}
 	wr.Snapshot = leases
-	wr.Cursor = cursor
+	return wr, nil
+}
+
+// networkWatchReset is called when incremental network watch failed and we need to grab a snapshot
+func (m *EtcdManager) networkWatchReset(ctx context.Context) (NetworkWatchResult, error) {
+	wr := NetworkWatchResult{}
+
+	networks, index, err := m.getNetworks(ctx)
+	if err != nil {
+		return wr, fmt.Errorf("failed to retrieve networks: %v", err)
+	}
+
+	wr.Cursor = watchCursor{index}
+	wr.Snapshot = networks
 	return wr, nil
 }
 

subnet/mock_registry.go
@@ -16,63 +16,79 @@ package subnet
 
 import (
 	"fmt"
+	"path"
+	"sort"
+	"strings"
 	"time"
 
 	etcd "github.com/coreos/flannel/Godeps/_workspace/src/github.com/coreos/etcd/client"
 	"github.com/coreos/flannel/Godeps/_workspace/src/golang.org/x/net/context"
 )
 
-type mockSubnetRegistry struct {
-	config  *etcd.Node
-	subnets *etcd.Node
-	events  chan *etcd.Response
-	index   uint64
-	ttl     time.Duration
+type MockSubnetRegistry struct {
+	networks map[string]*etcd.Node
+	events   chan *etcd.Response
+	index    uint64
+	ttl      time.Duration
 }
 
-func newMockRegistry(ttlOverride time.Duration, config string, initialSubnets []*etcd.Node) *mockSubnetRegistry {
+const networkKeyPrefix = "/coreos.com/network"
+
+func NewMockRegistry(ttlOverride time.Duration, network, config string, initialSubnets []*etcd.Node) *MockSubnetRegistry {
 	index := uint64(0)
+
+	node := &etcd.Node{Key: normalizeNetKey(network), Value: config, ModifiedIndex: 0, Nodes: make([]*etcd.Node, 0, 20)}
 	for _, n := range initialSubnets {
 		if n.ModifiedIndex > index {
 			index = n.ModifiedIndex
 		}
+		node.Nodes = append(node.Nodes, n)
 	}
 
-	return &mockSubnetRegistry{
-		config: &etcd.Node{
-			Value: config,
-		},
-		subnets: &etcd.Node{
-			Nodes: initialSubnets,
-		},
+	msr := &MockSubnetRegistry{
 		events: make(chan *etcd.Response, 1000),
 		index:  index + 1,
 		ttl:    ttlOverride,
 	}
+
+	msr.networks = make(map[string]*etcd.Node)
+	msr.networks[network] = node
+	return msr
 }
 
-func (msr *mockSubnetRegistry) getConfig(ctx context.Context, network string) (*etcd.Response, error) {
+func (msr *MockSubnetRegistry) getNetworkConfig(ctx context.Context, network string) (*etcd.Response, error) {
 	return &etcd.Response{
 		Index: msr.index,
-		Node:  msr.config,
+		Node:  msr.networks[network],
 	}, nil
 }
 
-func (msr *mockSubnetRegistry) setConfig(config string) {
-	msr.config = &etcd.Node{
-		Key:   "config",
-		Value: config,
+func (msr *MockSubnetRegistry) setConfig(network, config string) error {
+	n, ok := msr.networks[network]
+	if !ok {
+		return fmt.Errorf("Network %s not found", network)
 	}
+	n.Value = config
+	return nil
 }
 
-func (msr *mockSubnetRegistry) getSubnets(ctx context.Context, network string) (*etcd.Response, error) {
+func (msr *MockSubnetRegistry) getSubnets(ctx context.Context, network string) (*etcd.Response, error) {
+	n, ok := msr.networks[network]
+	if !ok {
+		return nil, fmt.Errorf("Network %s not found", network)
+	}
 	return &etcd.Response{
-		Node:  msr.subnets,
+		Node:  n,
 		Index: msr.index,
 	}, nil
 }
 
-func (msr *mockSubnetRegistry) createSubnet(ctx context.Context, network, sn, data string, ttl time.Duration) (*etcd.Response, error) {
+func (msr *MockSubnetRegistry) createSubnet(ctx context.Context, network, sn, data string, ttl time.Duration) (*etcd.Response, error) {
+	n, ok := msr.networks[network]
+	if !ok {
+		return nil, fmt.Errorf("Network %s not found", network)
+	}
+
 	msr.index += 1
 
 	if msr.ttl > 0 {
@@ -88,7 +104,7 @@ func (msr *mockSubnetRegistry) createSubnet(ctx context.Context, network, sn, da
 		Expiration:    &exp,
 	}
 
-	msr.subnets.Nodes = append(msr.subnets.Nodes, node)
+	n.Nodes = append(n.Nodes, node)
 	msr.events <- &etcd.Response{
 		Action: "add",
 		Node:   node,
@@ -100,23 +116,28 @@ func (msr *mockSubnetRegistry) createSubnet(ctx context.Context, network, sn, da
 	}, nil
 }
 
-func (msr *mockSubnetRegistry) updateSubnet(ctx context.Context, network, sn, data string, ttl time.Duration) (*etcd.Response, error) {
+func (msr *MockSubnetRegistry) updateSubnet(ctx context.Context, network, sn, data string, ttl time.Duration) (*etcd.Response, error) {
+	n, ok := msr.networks[network]
+	if !ok {
+		return nil, fmt.Errorf("Network %s not found", network)
+	}
+
 	msr.index += 1
 
 	exp := time.Now().Add(ttl)
 
-	for _, n := range msr.subnets.Nodes {
-		if n.Key == sn {
-			n.Value = data
-			n.ModifiedIndex = msr.index
-			n.Expiration = &exp
+	for _, sub := range n.Nodes {
+		if sub.Key == sn {
+			sub.Value = data
+			sub.ModifiedIndex = msr.index
+			sub.Expiration = &exp
 			msr.events <- &etcd.Response{
 				Action: "add",
-				Node:   n,
+				Node:   sub,
 			}
 
 			return &etcd.Response{
-				Node:  n,
+				Node:  sub,
 				Index: msr.index,
 			}, nil
 		}
@@ -125,20 +146,26 @@ func (msr *mockSubnetRegistry) updateSubnet(ctx context.Context, network, sn, da
 	return nil, fmt.Errorf("Subnet not found")
 }
 
-func (msr *mockSubnetRegistry) deleteSubnet(ctx context.Context, network, sn string) (*etcd.Response, error) {
+func (msr *MockSubnetRegistry) deleteSubnet(ctx context.Context, network, sn string) (*etcd.Response, error) {
+	n, ok := msr.networks[network]
+	if !ok {
+		return nil, fmt.Errorf("Network %s not found", network)
+	}
+
 	msr.index += 1
 
-	for i, n := range msr.subnets.Nodes {
-		if n.Key == sn {
-			msr.subnets.Nodes[i] = msr.subnets.Nodes[len(msr.subnets.Nodes)-1]
-			msr.subnets.Nodes = msr.subnets.Nodes[:len(msr.subnets.Nodes)-1]
+	for i, sub := range n.Nodes {
+		if sub.Key == sn {
+			n.Nodes[i] = n.Nodes[len(n.Nodes)-1]
+			n.Nodes = n.Nodes[:len(n.Nodes)-1]
+			sub.ModifiedIndex = msr.index
 			msr.events <- &etcd.Response{
 				Action: "delete",
-				Node:   n,
+				Node:   sub,
 			}
 
 			return &etcd.Response{
-				Node:  n,
+				Node:  sub,
 				Index: msr.index,
 			}, nil
 		}
@@ -148,8 +175,17 @@ func (msr *mockSubnetRegistry) deleteSubnet(ctx context.Context, network, sn str
 
 }
 
-func (msr *mockSubnetRegistry) watchSubnets(ctx context.Context, network string, since uint64) (*etcd.Response, error) {
+func (msr *MockSubnetRegistry) watch(ctx context.Context, network string, since uint64) (*etcd.Response, error) {
 	for {
+		if since < msr.index {
+			return nil, etcd.Error{
+				Code:    etcd.ErrorCodeEventIndexCleared,
+				Cause:   "out of date",
+				Message: "cursor is out of date",
+				Index:   msr.index,
+			}
+		}
+
 		select {
 		case <-ctx.Done():
 			return nil, ctx.Err()
@@ -163,27 +199,127 @@ func (msr *mockSubnetRegistry) watchSubnets(ctx context.Context, network string,
 	}
 }
 
-func (msr *mockSubnetRegistry) hasSubnet(sn string) bool {
-	for _, n := range msr.subnets.Nodes {
-		if n.Key == sn {
+func (msr *MockSubnetRegistry) hasSubnet(network, sn string) bool {
+	n, ok := msr.networks[network]
+	if !ok {
+		return false
+	}
+
+	for _, sub := range n.Nodes {
+		if sub.Key == sn {
 			return true
 		}
 	}
 	return false
 }
 
-func (msr *mockSubnetRegistry) expireSubnet(sn string) {
-	for i, n := range msr.subnets.Nodes {
-		if n.Key == sn {
+func (msr *MockSubnetRegistry) expireSubnet(network, sn string) {
+	n, ok := msr.networks[network]
+	if !ok {
+		return
+	}
+
+	for i, sub := range n.Nodes {
+		if sub.Key == sn {
 			msr.index += 1
-			msr.subnets.Nodes[i] = msr.subnets.Nodes[len(msr.subnets.Nodes)-1]
-			msr.subnets.Nodes = msr.subnets.Nodes[:len(msr.subnets.Nodes)-2]
-			n.ModifiedIndex = msr.index
+			n.Nodes[i] = n.Nodes[len(n.Nodes)-1]
+			n.Nodes = n.Nodes[:len(n.Nodes)-2]
+			sub.ModifiedIndex = msr.index
 			msr.events <- &etcd.Response{
 				Action: "expire",
-				Node:   n,
+				Node:   sub,
 			}
 			return
 		}
 	}
+}
+
+func (msr *MockSubnetRegistry) getNetworks(ctx context.Context) (*etcd.Response, error) {
+	var keys []string
+	for k := range msr.networks {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	networks := &etcd.Node{Key: networkKeyPrefix, Value: "", ModifiedIndex: msr.index, Nodes: make([]*etcd.Node, 0, len(keys))}
+	for _, k := range keys {
+		networks.Nodes = append(networks.Nodes, msr.networks[k])
+	}
+
+	return &etcd.Response{
+		Node:  networks,
+		Index: msr.index,
+	}, nil
+}
+
+func (msr *MockSubnetRegistry) getNetwork(ctx context.Context, network string) (*etcd.Response, error) {
+	n, ok := msr.networks[network]
+	if !ok {
+		return nil, fmt.Errorf("Network %s not found", network)
+	}
+
+	return &etcd.Response{
+		Node:  n,
+		Index: msr.index,
+	}, nil
+}
+
+func (msr *MockSubnetRegistry) CreateNetwork(ctx context.Context, network, config string) (*etcd.Response, error) {
+	_, ok := msr.networks[network]
+	if ok {
+		return nil, fmt.Errorf("Network %s already exists", network)
+	}
+
+	msr.index += 1
+
+	node := &etcd.Node{
+		Key:           normalizeNetKey(network),
+		Value:         config,
+		ModifiedIndex: msr.index,
+	}
+
+	msr.networks[network] = node
+	msr.events <- &etcd.Response{
+		Action: "add",
+		Node:   node,
+	}
+
+	return &etcd.Response{
+		Node:  node,
+		Index: msr.index,
+	}, nil
+}
+
+func (msr *MockSubnetRegistry) DeleteNetwork(ctx context.Context, network string) (*etcd.Response, error) {
+	n, ok := msr.networks[network]
+	if !ok {
+		return nil, fmt.Errorf("Network %s not found", network)
+	}
+
+	msr.index += 1
+
+	n.ModifiedIndex = msr.index
+
+	delete(msr.networks, network)
+	msr.events <- &etcd.Response{
+		Action: "delete",
+		Node:   n,
+	}
+
+	return &etcd.Response{
+		Node:  n,
+		Index: msr.index,
+	}, nil
+}
+
+func normalizeNetKey(key string) string {
+	match := networkKeyPrefix
+	newKey := key
+	if !strings.HasPrefix(newKey, match+"/") {
+		newKey = path.Join(match, key)
+	}
+	if !strings.HasSuffix(newKey, "/config") {
+		newKey = path.Join(newKey, "config")
+	}
+	return newKey
 }

backend/hostgw/hostgw.go
@@ -136,7 +136,7 @@ func (rb *HostgwBackend) Name() string {
 func (rb *HostgwBackend) handleSubnetEvents(batch []subnet.Event) {
 	for _, evt := range batch {
 		switch evt.Type {
-		case subnet.SubnetAdded:
+		case subnet.EventAdded:
 			log.Infof("Subnet added: %v via %v", evt.Lease.Subnet, evt.Lease.Attrs.PublicIP)
 
 			if evt.Lease.Attrs.BackendType != "host-gw" {
@@ -155,7 +155,7 @@ func (rb *HostgwBackend) handleSubnetEvents(batch []subnet.Event) {
 			}
 			rb.addToRouteList(route)
 
-		case subnet.SubnetRemoved:
+		case subnet.EventRemoved:
 			log.Info("Subnet removed: ", evt.Lease.Subnet)
 
 			if evt.Lease.Attrs.BackendType != "host-gw" {

backend/udp/udp.go
@@ -243,12 +243,12 @@ func (m *UdpBackend) monitorEvents() {
 func (m *UdpBackend) processSubnetEvents(batch []subnet.Event) {
 	for _, evt := range batch {
 		switch evt.Type {
-		case subnet.SubnetAdded:
+		case subnet.EventAdded:
 			log.Info("Subnet added: ", evt.Lease.Subnet)
 
 			setRoute(m.ctl, evt.Lease.Subnet, evt.Lease.Attrs.PublicIP, m.cfg.Port)
 
-		case subnet.SubnetRemoved:
+		case subnet.EventRemoved:
 			log.Info("Subnet removed: ", evt.Lease.Subnet)
 
 			removeRoute(m.ctl, evt.Lease.Subnet)

backend/vxlan/vxlan.go
@@ -228,7 +228,7 @@ type vxlanLeaseAttrs struct {
 func (vb *VXLANBackend) handleSubnetEvents(batch []subnet.Event) {
 	for _, evt := range batch {
 		switch evt.Type {
-		case subnet.SubnetAdded:
+		case subnet.EventAdded:
 			log.Info("Subnet added: ", evt.Lease.Subnet)
 
 			if evt.Lease.Attrs.BackendType != "vxlan" {
@@ -244,7 +244,7 @@ func (vb *VXLANBackend) handleSubnetEvents(batch []subnet.Event) {
 			vb.rts.set(evt.Lease.Subnet, net.HardwareAddr(attrs.VtepMAC))
 			vb.dev.AddL2(neigh{IP: evt.Lease.Attrs.PublicIP, MAC: net.HardwareAddr(attrs.VtepMAC)})
 
-		case subnet.SubnetRemoved:
+		case subnet.EventRemoved:
 			log.Info("Subnet removed: ", evt.Lease.Subnet)
 
 			if evt.Lease.Attrs.BackendType != "vxlan" {

remote/client.go
@@ -164,13 +164,11 @@ func (m *RemoteManager) RenewLease(ctx context.Context, network string, lease *s
 	return nil
 }
 
-func (m *RemoteManager) WatchLeases(ctx context.Context, network string, cursor interface{}) (subnet.WatchResult, error) {
-	url := m.mkurl(network, "leases")
-
+func (m *RemoteManager) watch(ctx context.Context, url string, cursor interface{}, wr interface{}) error {
 	if cursor != nil {
 		c, ok := cursor.(string)
 		if !ok {
-			return subnet.WatchResult{}, fmt.Errorf("internal error: RemoteManager.WatchLeases received non-string cursor")
+			return fmt.Errorf("internal error: RemoteManager.watch received non-string cursor")
 		}
 
 		url = fmt.Sprintf("%v?next=%v", url, c)
@@ -178,19 +176,44 @@ func (m *RemoteManager) WatchLeases(ctx context.Context, network string, cursor 
 
 	resp, err := m.httpGet(ctx, url)
 	if err != nil {
-		return subnet.WatchResult{}, err
+		return err
 	}
 
 	if resp.StatusCode != http.StatusOK {
-		return subnet.WatchResult{}, httpError(resp)
+		return httpError(resp)
 	}
 
-	wr := subnet.WatchResult{}
-	if err := json.NewDecoder(resp.Body).Decode(&wr); err != nil {
-		return subnet.WatchResult{}, err
+	if err := json.NewDecoder(resp.Body).Decode(wr); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (m *RemoteManager) WatchLeases(ctx context.Context, network string, cursor interface{}) (subnet.LeaseWatchResult, error) {
+	url := m.mkurl(network, "leases")
+
+	wr := subnet.LeaseWatchResult{}
+	err := m.watch(ctx, url, cursor, &wr)
+	if err != nil {
+		return subnet.LeaseWatchResult{}, err
 	}
 	if _, ok := wr.Cursor.(string); !ok {
-		return subnet.WatchResult{}, fmt.Errorf("lease watch returned non-string cursor")
+		return subnet.LeaseWatchResult{}, fmt.Errorf("watch returned non-string cursor")
+	}
+
+	return wr, nil
+}
+
+func (m *RemoteManager) WatchNetworks(ctx context.Context, cursor interface{}) (subnet.NetworkWatchResult, error) {
+	wr := subnet.NetworkWatchResult{}
+	err := m.watch(ctx, m.base+"/", cursor, &wr)
+	if err != nil {
+		return subnet.NetworkWatchResult{}, err
+	}
+
+	if _, ok := wr.Cursor.(string); !ok {
+		return subnet.NetworkWatchResult{}, fmt.Errorf("watch returned non-string cursor")
 	}
 
 	return wr, nil

remote/remote_test.go
@@ -33,7 +33,8 @@ const expectedNetwork = "10.1.0.0/16"
 
 func TestRemote(t *testing.T) {
 	config := fmt.Sprintf(`{"Network": %q}`, expectedNetwork)
-	sm := subnet.NewMockManager(1, config)
+	serverRegistry := subnet.NewMockRegistry(1, "", config, nil)
+	sm := subnet.NewMockManager(serverRegistry)
 
 	addr := "127.0.0.1:9999"
 
@@ -45,7 +46,7 @@ func TestRemote(t *testing.T) {
 		wg.Done()
 	}()
 
-	doTestRemote(ctx, t, addr)
+	doTestRemote(ctx, t, addr, serverRegistry)
 
 	cancel()
 	wg.Wait()
@@ -76,7 +77,7 @@ func isConnRefused(err error) bool {
 	return false
 }
 
-func doTestRemote(ctx context.Context, t *testing.T, remoteAddr string) {
+func doTestRemote(ctx context.Context, t *testing.T, remoteAddr string, serverRegistry *subnet.MockSubnetRegistry) {
 	sm, err := NewRemoteManager(remoteAddr, "", "", "")
 	if err != nil {
 		t.Fatalf("Failed to create remote mananager: %v", err)
@@ -120,10 +121,12 @@ func doTestRemote(ctx context.Context, t *testing.T, remoteAddr string) {
 		t.Errorf("RenewLease failed: %v", err)
 	}
 
-	doTestWatch(t, sm)
+	doTestWatchLeases(t, sm)
+
+	doTestWatchNetworks(t, sm, serverRegistry)
 }
 
-func doTestWatch(t *testing.T, sm subnet.Manager) {
+func doTestWatchLeases(t *testing.T, sm subnet.Manager) {
 	ctx, cancel := context.WithCancel(context.Background())
 	wg := sync.WaitGroup{}
 	wg.Add(1)
@@ -160,11 +163,72 @@ func doTestWatch(t *testing.T, sm subnet.Manager) {
 	}
 
 	evt := evtBatch[0]
-	if evt.Type != subnet.SubnetAdded {
+	if evt.Type != subnet.EventAdded {
 		t.Fatalf("WatchSubnets produced wrong event type")
 	}
 
 	if evt.Lease.Key() != l.Key() {
 		t.Errorf("WatchSubnet produced wrong subnet: expected %s, got %s", l.Key(), evt.Lease.Key())
+	}
+}
+
+func doTestWatchNetworks(t *testing.T, sm subnet.Manager, serverRegistry *subnet.MockSubnetRegistry) {
+	ctx, cancel := context.WithCancel(context.Background())
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	defer func() {
+		cancel()
+		wg.Wait()
+	}()
+
+	events := make(chan []subnet.Event)
+	go func() {
+		subnet.WatchNetworks(ctx, sm, events)
+		wg.Done()
+	}()
+
+	// skip over the initial snapshot
+	<-events
+
+	expectedNetname := "foobar"
+	config := fmt.Sprintf(`{"Network": %q}`, expectedNetwork)
+	_, err := serverRegistry.CreateNetwork(ctx, expectedNetname, config)
+	if err != nil {
+		t.Errorf("create network failed: %v", err)
+	}
+
+	evtBatch := <-events
+
+	if len(evtBatch) != 1 {
+		t.Fatalf("WatchNetworks create produced wrong sized event batch")
+	}
+
+	evt := evtBatch[0]
+	if evt.Type != subnet.EventAdded {
+		t.Fatalf("WatchNetworks create produced wrong event type")
+	}
+
+	if evt.Network != expectedNetname {
+		t.Errorf("WatchNetwork create produced wrong network: expected %s, got %s", expectedNetname, evt.Network)
+	}
+
+	_, err = serverRegistry.DeleteNetwork(ctx, expectedNetname)
+	if err != nil {
+		t.Errorf("delete network failed: %v", err)
+	}
+
+	evtBatch = <-events
+
+	if len(evtBatch) != 1 {
+		t.Fatalf("WatchNetworks delete produced wrong sized event batch")
+	}
+
+	evt = evtBatch[0]
+	if evt.Type != subnet.EventRemoved {
+		t.Fatalf("WatchNetworks delete produced wrong event type")
+	}
+
+	if evt.Network != expectedNetname {
+		t.Errorf("WatchNetwork delete produced wrong network: expected %s, got %s", expectedNetname, evt.Network)
 	}
 }

remote/server.go
@@ -152,6 +152,32 @@ func handleWatchLeases(ctx context.Context, sm subnet.Manager, w http.ResponseWr
 	jsonResponse(w, http.StatusOK, wr)
 }
 
+// GET /?next=cursor watches
+// GET / retrieves all networks
+func handleNetworks(ctx context.Context, sm subnet.Manager, w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+
+	cursor := getCursor(r.URL)
+	wr, err := sm.WatchNetworks(ctx, cursor)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	switch wr.Cursor.(type) {
+	case string:
+	case fmt.Stringer:
+		wr.Cursor = wr.Cursor.(fmt.Stringer).String()
+	default:
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w, fmt.Errorf("internal error: watch cursor is of unknown type"))
+		return
+	}
+
+	jsonResponse(w, http.StatusOK, wr)
+}
+
 func bindHandler(h handler, ctx context.Context, sm subnet.Manager) http.HandlerFunc {
 	return func(resp http.ResponseWriter, req *http.Request) {
 		h(ctx, sm, resp, req)
@@ -237,6 +263,7 @@ func RunServer(ctx context.Context, sm subnet.Manager, listenAddr, cafile, certf
 	r.HandleFunc("/v1/{network}/leases", bindHandler(handleAcquireLease, ctx, sm)).Methods("POST")
 	r.HandleFunc("/v1/{network}/leases/{subnet}", bindHandler(handleRenewLease, ctx, sm)).Methods("PUT")
 	r.HandleFunc("/v1/{network}/leases", bindHandler(handleWatchLeases, ctx, sm)).Methods("GET")
+	r.HandleFunc("/v1/", bindHandler(handleNetworks, ctx, sm)).Methods("GET")
 
 	l, err := listener(listenAddr, cafile, certfile, keyfile)
 	if err != nil {

commit 40737d1ab13c1bd8dd1bea7eff1c6b9f68f3a08e

Fix etcd implementation of getNetworks()
etcd itself returns networks in a different format than the code and
the testcases expected.  Fix up the code and testcases to match what
etcd returns.

Fixes: d9d89f6bde8dfeb8af252f39ee70e2ecfc379eee (subnet: add infrastructure and tests for network watches)

subnet/etcd.go
@@ -462,9 +462,12 @@ func (m *EtcdManager) getNetworks(ctx context.Context) ([]string, uint64, error)
 
 	if err == nil {
 		for _, node := range resp.Node.Nodes {
-			netname, err := m.parseNetworkKey(node.Key)
-			if err == nil {
-				networks = append(networks, netname)
+			// Look for '/config' on the child nodes
+			for _, child := range node.Nodes {
+				netname, err := m.parseNetworkKey(child.Key)
+				if err == nil {
+					networks = append(networks, netname)
+				}
 			}
 		}
 

subnet/mock_registry.go
@@ -234,6 +234,13 @@ func (msr *MockSubnetRegistry) expireSubnet(network, sn string) {
 	}
 }
 
+func configKeyToNetworkKey(configKey string) string {
+	if !strings.HasSuffix(configKey, "/config") {
+		return ""
+	}
+	return strings.TrimSuffix(configKey, "/config")
+}
+
 func (msr *MockSubnetRegistry) getNetworks(ctx context.Context) (*etcd.Response, error) {
 	var keys []string
 	for k := range msr.networks {
@@ -241,13 +248,34 @@ func (msr *MockSubnetRegistry) getNetworks(ctx context.Context) (*etcd.Response,
 	}
 	sort.Strings(keys)
 
-	networks := &etcd.Node{Key: networkKeyPrefix, Value: "", ModifiedIndex: msr.index, Nodes: make([]*etcd.Node, 0, len(keys))}
+	// mimic etcd by returning a multi-level response where each top-level
+	// node is a network node, and each network node has a single child
+	// 'config' node.  eg:
+	//
+	//  /coreos.com/network
+	//     /coreos.com/network/blue
+	//        /coreos.com/network/blue/config
+	//     /coreos.com/network/red
+	//        /coreos.com/network/red/config
+	//
+	toplevel := &etcd.Node{Key: networkKeyPrefix, Value: "", ModifiedIndex: msr.index, Nodes: make([]*etcd.Node, 0, len(keys))}
 	for _, k := range keys {
-		networks.Nodes = append(networks.Nodes, msr.networks[k])
+		netKey := configKeyToNetworkKey(msr.networks[k].Key)
+		if netKey == "" {
+			continue
+		}
+		network := &etcd.Node{
+			Key:           netKey,
+			Value:         "",
+			ModifiedIndex: msr.index,
+			Nodes:         []*etcd.Node{msr.networks[k]},
+		}
+
+		toplevel.Nodes = append(toplevel.Nodes, network)
 	}
 
 	return &etcd.Response{
-		Node:  networks,
+		Node:  toplevel,
 		Index: msr.index,
 	}, nil
 }