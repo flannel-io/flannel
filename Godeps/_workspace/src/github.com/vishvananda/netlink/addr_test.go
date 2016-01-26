416ec9777dbb436b97375df44788290ac6390819
Takashi Okamoto <toraneko@gmail.com>
2016-01-27 00:25:18 +0900

    Revendor netlink library

--- Godeps/_workspace/src/github.com/vishvananda/netlink/addr_test.go
+++ Godeps/_workspace/src/github.com/vishvananda/netlink/addr_test.go
@@ -1,10 +1,44 @@
 package netlink
 
 import (
+	"net"
+	"os"
+	"syscall"
 	"testing"
 )
 
-func TestAddrAddDel(t *testing.T) {
+func TestAddr(t *testing.T) {
+	if os.Getenv("TRAVIS_BUILD_DIR") != "" {
+		t.Skipf("Fails in travis with: addr_test.go:68: Address flags not set properly, got=0, expected=128")
+	}
+	// TODO: IFA_F_PERMANENT does not seem to be set by default on older kernels?
+	var address = &net.IPNet{net.IPv4(127, 0, 0, 2), net.CIDRMask(24, 32)}
+	var addrTests = []struct {
+		addr     *Addr
+		expected *Addr
+	}{
+		{
+			&Addr{IPNet: address},
+			&Addr{IPNet: address, Label: "lo", Scope: syscall.RT_SCOPE_UNIVERSE, Flags: syscall.IFA_F_PERMANENT},
+		},
+		{
+			&Addr{IPNet: address, Label: "local"},
+			&Addr{IPNet: address, Label: "local", Scope: syscall.RT_SCOPE_UNIVERSE, Flags: syscall.IFA_F_PERMANENT},
+		},
+		{
+			&Addr{IPNet: address, Flags: syscall.IFA_F_OPTIMISTIC},
+			&Addr{IPNet: address, Label: "lo", Flags: syscall.IFA_F_OPTIMISTIC | syscall.IFA_F_PERMANENT, Scope: syscall.RT_SCOPE_UNIVERSE},
+		},
+		{
+			&Addr{IPNet: address, Flags: syscall.IFA_F_OPTIMISTIC | syscall.IFA_F_DADFAILED},
+			&Addr{IPNet: address, Label: "lo", Flags: syscall.IFA_F_OPTIMISTIC | syscall.IFA_F_DADFAILED | syscall.IFA_F_PERMANENT, Scope: syscall.RT_SCOPE_UNIVERSE},
+		},
+		{
+			&Addr{IPNet: address, Scope: syscall.RT_SCOPE_NOWHERE},
+			&Addr{IPNet: address, Label: "lo", Flags: syscall.IFA_F_PERMANENT, Scope: syscall.RT_SCOPE_NOWHERE},
+		},
+	}
+
 	tearDown := setUpNetlinkTest(t)
 	defer tearDown()
 
@@ -13,33 +47,47 @@ func TestAddrAddDel(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	addr, err := ParseAddr("127.1.1.1/24 local")
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, tt := range addrTests {
+		if err = AddrAdd(link, tt.addr); err != nil {
+			t.Fatal(err)
+		}
 
-	if err = AddrAdd(link, addr); err != nil {
-		t.Fatal(err)
-	}
+		addrs, err := AddrList(link, FAMILY_ALL)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	addrs, err := AddrList(link, FAMILY_ALL)
-	if err != nil {
-		t.Fatal(err)
-	}
+		if len(addrs) != 1 {
+			t.Fatal("Address not added properly")
+		}
 
-	if len(addrs) != 1 || !addr.Equal(addrs[0]) || addrs[0].Label != addr.Label {
-		t.Fatal("Address not added properly")
-	}
+		if !addrs[0].Equal(*tt.expected) {
+			t.Fatalf("Address ip no set properly, got=%s, expected=%s", addrs[0], tt.expected)
+		}
 
-	if err = AddrDel(link, addr); err != nil {
-		t.Fatal(err)
-	}
-	addrs, err = AddrList(link, FAMILY_ALL)
-	if err != nil {
-		t.Fatal(err)
-	}
+		if addrs[0].Label != tt.expected.Label {
+			t.Fatalf("Address label not set properly, got=%s, expected=%s", addrs[0].Label, tt.expected.Label)
+		}
 
-	if len(addrs) != 0 {
-		t.Fatal("Address not removed properly")
+		if addrs[0].Flags != tt.expected.Flags {
+			t.Fatalf("Address flags not set properly, got=%d, expected=%d", addrs[0].Flags, tt.expected.Flags)
+		}
+
+		if addrs[0].Scope != tt.expected.Scope {
+			t.Fatalf("Address scope not set properly, got=%d, expected=%d", addrs[0].Scope, tt.expected.Scope)
+		}
+
+		if err = AddrDel(link, tt.addr); err != nil {
+			t.Fatal(err)
+		}
+
+		addrs, err = AddrList(link, FAMILY_ALL)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if len(addrs) != 0 {
+			t.Fatal("Address not removed properly")
+		}
 	}
 }
