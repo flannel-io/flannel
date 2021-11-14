61d22174813aba2f295a23031c25ea3c1976713c
Sjoerd Simons <sjoerd@collabora.com>
2021-11-14 20:06:43 +0100

    ip: Add helpers to determine if an IP address is private
    Add IsPrivate methods to IP4 and IP6 to determine if a ip address is in
    the private range. similar to IsPrivate of net.IP in newer golang
    versions.
    
    Signed-off-by: Sjoerd Simons <sjoerd@collabora.com>

--- pkg/ip/ip6net.go
+++ pkg/ip/ip6net.go
@@ -111,6 +111,12 @@ func (ip6 *IP6) Cmp(other *IP6) int {
 	return (*big.Int)(ip6).Cmp((*big.Int)(other))
 }
 
+// IsPrivate reports whether ip is a private address, according to
+// RFC 4193 (IPv6 addresses).
+func (ip6 *IP6) IsPrivate() bool {
+	return (*big.Int)(ip6).Bytes()[0]&0xfe == 0xfc
+}
+
 // similar to net.IPNet but has uint based representation
 type IP6Net struct {
 	IP        *IP6
