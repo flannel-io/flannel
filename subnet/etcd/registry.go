7e716be97f72ad7f0c6fa1dc92d9d67c523f477c
ttyS3 <ttys3.rust@gmail.com>
2022-10-21 02:45:07 +0800

    fix: do not return "can't connect to etcd: no cert file found" error, allow connect with etcd without cert

--- subnet/etcd/registry.go
+++ subnet/etcd/registry.go
@@ -79,7 +79,7 @@ func newTlsConfig(c *EtcdConfig) (*tls.Config, error) {
 	}
 
 	if c.Keyfile == "" || c.Certfile == "" {
-		return nil, fmt.Errorf("can't connect to etcd: no cert file found")
+		return nil, nil
 	} else {
 		cert, err := tlsutil.NewCert(c.Certfile, c.Keyfile, nil)
 		if err != nil {
